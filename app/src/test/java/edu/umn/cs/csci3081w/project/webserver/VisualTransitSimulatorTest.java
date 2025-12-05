10cdad5b9e1d7dcbe31e1231afd52409971fab54
Sanghwi Park <sanghwi20050418@gmail.com>
2025-12-05 17:52:47 -0600

    setUp added

--- app/src/test/java/edu/umn/cs/csci3081w/project/webserver/VisualTransitSimulatorTest.java
+++ app/src/test/java/edu/umn/cs/csci3081w/project/webserver/VisualTransitSimulatorTest.java
@@ -1,4 +1,21 @@
 package edu.umn.cs.csci3081w.project.webserver;
 
+import edu.umn.cs.csci3081w.project.model.PassengerFactory;
+import edu.umn.cs.csci3081w.project.model.RandomPassengerGenerator;
+import edu.umn.cs.csci3081w.project.model.Vehicle;
+import org.junit.jupiter.api.BeforeEach;
+
 public class VisualTransitSimulatorTest {
+
+  /**
+   * Setup deterministic operations before each test runs.
+   */
+  @BeforeEach
+  public void setUp() {
+    PassengerFactory.DETERMINISTIC = true;
+    PassengerFactory.DETERMINISTIC_NAMES_COUNT = 0;
+    PassengerFactory.DETERMINISTIC_DESTINATION_COUNT = 0;
+    RandomPassengerGenerator.DETERMINISTIC = true;
+    Vehicle.TESTING = true;
+  }
 }
