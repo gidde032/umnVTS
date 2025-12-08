a90f2a0612421da0e0d763891df1444e702ad793
Jiawen <deng0324@umn.edu>
2025-12-07 20:16:59 -0600

    tests updated to improve branch coverage for WebServerSession

--- app/src/test/java/edu/umn/cs/csci3081w/project/webserver/WebServerSessionTest.java
+++ app/src/test/java/edu/umn/cs/csci3081w/project/webserver/WebServerSessionTest.java
@@ -1,15 +1,21 @@
 package edu.umn.cs.csci3081w.project.webserver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.google.gson.JsonObject;
 import edu.umn.cs.csci3081w.project.model.PassengerFactory;
 import edu.umn.cs.csci3081w.project.model.RandomPassengerGenerator;
-import edu.umn.cs.csci3081w.project.model.Vehicle;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import javax.websocket.RemoteEndpoint;
 import javax.websocket.Session;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +23,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 public class WebServerSessionTest {
+
   /**
    * Setup deterministic operations before each test runs.
    */
@@ -62,7 +69,78 @@ public class WebServerSessionTest {
     JsonObject unknownCommand = new JsonObject();
     unknownCommand.addProperty("command", "thisIsNotARealCommand");
 
-    // If onMessage throws an exception, the test will fail automatically.
     webServerSessionSpy.onMessage(unknownCommand.toString());
+  }
+
+  /**
+   * Tests that sendJson successfully sends a message when no IOException is thrown.
+   */
+  @Test
+  public void testSendJsonSuccess() throws IOException {
+    WebServerSession webServerSession = new WebServerSession();
+
+    Session sessionMock = mock(Session.class);
+    RemoteEndpoint.Basic basicMock = mock(RemoteEndpoint.Basic.class);
+
+    when(sessionMock.getBasicRemote()).thenReturn(basicMock);
+
+    webServerSession.onOpen(sessionMock);
+
+    JsonObject data = new JsonObject();
+    data.addProperty("key", "value");
+
+    webServerSession.sendJson(data);
+
+    verify(basicMock).sendText(data.toString());
+  }
+
+  /**
+   * Tests that sendJson handles IOException from the underlying session.
+   *
+   * <p>This exercises the catch block in sendJson.
+   */
+  @Test
+  public void testSendJsonHandlesIOException() throws IOException {
+    WebServerSession webServerSession = new WebServerSession();
+
+    Session sessionMock = mock(Session.class);
+    RemoteEndpoint.Basic basicMock = mock(RemoteEndpoint.Basic.class);
+
+    when(sessionMock.getBasicRemote()).thenReturn(basicMock);
+    doThrow(new IOException("forced failure")).when(basicMock).sendText(anyString());
+
+    webServerSession.onOpen(sessionMock);
+
+    JsonObject data = new JsonObject();
+    data.addProperty("key", "value");
+
+    webServerSession.sendJson(data);
+  }
+
+  /**
+   * Tests that onError can be called without throwing an exception.
+   */
+  @Test
+  public void testOnErrorDoesNotThrow() {
+    WebServerSession webServerSession = new WebServerSession();
+    webServerSession.onError(new RuntimeException("test error"));
+  }
+
+  /**
+   * Tests that onClose clears the session field.
+   */
+  @Test
+  public void testOnCloseClearsSession() throws Exception {
+    WebServerSession webServerSession = new WebServerSession();
+    Session sessionMock = mock(Session.class);
+
+    webServerSession.onOpen(sessionMock);
+    webServerSession.onClose(sessionMock);
+
+    Field sessionField = WebServerSession.class.getDeclaredField("session");
+    sessionField.setAccessible(true);
+    Object internalSession = sessionField.get(webServerSession);
+
+    assertNull(internalSession);
   }
 }
